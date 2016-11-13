24295d89523138a2db235376f36693661f32d670
mibzman <27thisotope@gmail.com>
2016-11-13 10:17:48 -0500

    switched buckets to vector

--- HashTable/HashChain.cpp
+++ HashTable/HashChain.cpp
@@ -1,6 +1,128 @@
 #include "HashChain.hpp"
+#include <iostream>
+
+void TestAdd(){
+	Bucket bucket;
+	bucket.Add(4);
+	//std::cout << "yay";
+}
+
+void TestBegin(){
+	Bucket bucket;
+        bucket.Add(4);
+	bucket.Add(6);
+	auto test = bucket.Begin();
+	if(*test != 4){
+		std::cout << "TestGet broke" << std::endl;
+	}
+}
+
+void TestEnd(){
+	Bucket bucket;
+        bucket.Add(4);
+        bucket.Add(6);
+        auto test = bucket.End();
+        if(*test != 6){
+                std::cout << "TestEnd broke" << std::endl;
+        }
+
+}
+
+void TestFind(){
+	Bucket bucket;
+	bucket.Add(4);
+        bucket.Add(6);
+	int* found = bucket.Find(6);
+
+	if (!found){
+                std::cout << "TestEnd broke" << std::endl;
+        }
+
+	if (*found != 6){
+		std::cout << "TestEnd broke" << std::endl;
+	}
+}
+
+void BucketTest(){
+	TestAdd();
+	TestBegin();
+	TestEnd();
+}
+
+void TestInsert(){
+	SepHash table;
+	table.insert(5);
+	table.insert(10);
+	if (*table.Table[5].Begin() != 5){
+		 std::cout << "TestInsert broke" << std::endl;
+	}
+
+	if (*table.Table[2].Begin() != 10){
+                 std::cout << "TestInsert broke" << std::endl;
+        }
+}
+
+void TestRehash(){
+	SepHash table;
+	std::vector<int> values = {50, 25, 100, 10, 75, 76, 74, 4, 99};
+	for (int i = 0; i < values.size(); i++){
+		table.insert(values[i]);
+	}
+
+	if (*table.Table[2].Begin() != 50){
+                 std::cout << "TestRehash broke" << std::endl;
+        }
+
+	if (*table.Table[9].Begin() != 25){
+                 std::cout << "TestRehash broke" << std::endl;
+        }
+}
+
+void TestCopyConstruct(){
+	SepHash table;
+        table.insert(5);
+        table.insert(10);
+	SepHash newTable = SepHash(table);
+
+	if (*newTable.Table[5].Begin() != 5){
+                 std::cout << "TestCopyConstruct broke" << std::endl;
+        }
+
+        if (*newTable.Table[2].Begin() != 10){
+                 std::cout << "TestCopyConstruct broke" << std::endl;
+        }
+}
+
+void TestCopyAssign(){
+        SepHash table;
+        table.insert(5);
+        table.insert(10);
+        SepHash newTable;
+	newTable.insert(50);
+        newTable.insert(100);
+
+	newTable = table;
+
+        if (*newTable.Table[5].Begin() != 5){
+                 std::cout << "TestCopyAssign broke" << std::endl;
+        }
+
+        if (*newTable.Table[2].Begin() != 10){
+                 std::cout << "TestCopyAssign broke" << std::endl;
+        }
+}
+
+
+void SepHashTest(){
+	TestInsert();
+	TestRehash();
+	TestCopyConstruct();
+	TestCopyAssign();
+}
 
 int main(){
 	//do stuff
+	BucketTest();
+	SepHashTest();
 	return 0;
 }
--- HashTable/HashChain.hpp
+++ HashTable/HashChain.hpp
@@ -1,4 +1,116 @@
-template<typename T>
-struct SepHash {//a seperate chained hash table
+#include <vector>
+#include <list>
+#include <iostream>
+
+struct Bucket {
+	std::vector<int> List;
+	int Size = 0;
 	
-}
+	void Add(int val){
+		List.push_back(val);
+		Size++;
+	}
+	
+	//TODO: implement me!
+	int* Find(int val){
+		int* output;
+		for (int i = 0; i < Size; i++){
+			output = &List[i];
+			if (*output == val){
+				return output;
+			}
+		}
+		return nullptr;
+		/*
+		int* otuput = List.begin();
+		while(output < List.end()){
+			if (val == *output){
+				return output;
+			}
+			output++;
+		}
+		return nullptr;
+		*/
+	}
+
+
+	//I do this so I can play with using a linked list vs a vector without changing the hash table code
+	int* Begin(){
+		return &List[0];
+	}
+
+	int* End(){ 
+                return &List[Size-1];
+        }
+
+};
+
+struct SepHash {//a seperate chained hash table
+	private:
+
+	int KeyCount;
+
+	int Hash(int key){
+		return key % bucket_count();
+	}
+	public:
+	std::vector<Bucket> Table;
+
+	SepHash(){
+		KeyCount = 0;
+		Bucket emptyBucket = Bucket();
+		std::vector<Bucket> newTable(8, emptyBucket);
+                Table = newTable;
+	}
+
+	SepHash(const SepHash & other){
+		KeyCount = other.key_count();
+		Table = other.Table;
+	}
+
+	SepHash & operator=(const SepHash & other){
+		KeyCount = other.key_count();
+                Table = other.Table;
+	}
+
+	int load() const{
+		return key_count() / bucket_count();
+	}
+
+	int key_count() const {
+		return KeyCount;
+	}
+
+	int bucket_count() const{
+		return Table.size();
+	}
+
+	void insert(int key){
+		int hash = Hash(key);
+		if (load() > .75){
+			rehash(bucket_count() * 2);
+		}
+		Table[hash].Add(key);
+		KeyCount++;
+	}
+
+	void rehash(int size){
+		Bucket emptyBucket = Bucket();
+		std::vector<Bucket> oldTable = Table; 
+		//we have to do it this way to make the hash function work right
+		KeyCount = 0;
+		std::vector<Bucket> newTable(size, emptyBucket);
+		Table = newTable;
+		for (int i = 0; i < oldTable.size(); i++){
+			Bucket thisBucket = oldTable[i];
+			for (int i = 0; i < thisBucket.Size; i++){
+				int newHash = Hash(thisBucket.List[i]);
+				Table[newHash].Add(thisBucket.List[i]);
+				KeyCount++;
+			}
+		}
+	}
+	
+	
+
+};
